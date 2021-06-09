92d4f9fab0a562b9342ead6466f88309862f71a1
everglow <78077569+okchanho@users.noreply.github.com>
2021-06-10 08:48:10 +0900

    fist git
    first git

---
+++ mooyaho/src/test/java/com/example/mooyaho/MooyahoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mooyaho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooyahoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
