09cf95a4c8dd068904c2eb9b33d317e8e3d4ed0e
everglow <78077569+okchanho@users.noreply.github.com>
2021-06-22 08:22:41 +0900

    6/21
    6/21

--- mooyaho/src/main/java/com/example/mooyaho/user/UserController.java
+++ mooyaho/src/main/java/com/example/mooyaho/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
+    //연습입니다.
     @GetMapping("hi")
     public String hi(){
         return "hi.html";
