d7f5216132349a024eb84ec9e271f9836b3e2fce
everglow <78077569+okchanho@users.noreply.github.com>
2021-06-28 20:25:36 +0900

    6/28

---
+++ facebookClone1.1.2/src/main/java/com/koreait/facebook/feed/FeedController.java
@@ -0,0 +1,18 @@
+package com.koreait.facebook.feed;
+
+import com.koreait.facebook.feed.model.FeedEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feed")
+public class FeedController {
+
+    @GetMapping("/home")
+    public void home() {}
+
+    @GetMapping("/reg")
+    public void reg(FeedEntity obj) {}
+}
