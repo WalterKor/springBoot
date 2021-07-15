4e9f61ee4a33f8f08ea128a019f14f72acfd617f
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-15 09:11:13 +0900

    7/15
    카카오 소셜로그인

---
+++ facebookClone1.1.9/src/main/java/com/koreait/facebook/user/model/UserEntity.java
@@ -0,0 +1,22 @@
+package com.koreait.facebook.user.model;
+
+import lombok.*;
+import org.apache.ibatis.type.Alias;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    private int iuser;
+    private String provider;
+    private String email;
+    private String pw;
+    private String nm;
+    private String tel;
+    private String authCd;
+    private String mainProfile;
+    private String regdt;
+}
