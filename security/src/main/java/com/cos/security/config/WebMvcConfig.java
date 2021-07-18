637e96ffeb789d128cc7ed863606e1da9870ec59
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-18 14:48:01 +0900

    7/18

--- security/src/main/java/com/cos/security/config/WebMvcConfig.java
+++ security/src/main/java/com/cos/security/config/WebMvcConfig.java
@@ -17,5 +17,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setPrefix("classpath:/templates/");
         resolver.setSuffix(".html");
         registry.viewResolver(resolver);
+
     }
 }
--- security/src/main/java/com/cos/security/oauth/PricipalOauth2UserService.java
+++ security/src/main/java/com/cos/security/oauth/PricipalOauth2UserService.java
@@ -12,8 +12,8 @@ public class PricipalOauth2UserService extends DefaultOAuth2UserService {
     //구글로 부터 받은 userRequest 데이터에대한 후처리되는 함수
     @Override //userRequest 토큰이 이렇게 리턴된다.
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        System.out.println("userRequest :" +userRequest.getClientRegistration());//registrationId로 어떤 OAuth로 로그인했는지확인 가능
-        System.out.println("userRequest :" +userRequest.getAccessToken());
+        System.out.println("userRequest.getClientRegistration() :" +userRequest.getClientRegistration());//registrationId로 어떤 OAuth로 로그인했는지확인 가능
+        System.out.println("userRequest.getAccessToken() :" + userRequest.getAccessToken());
         // 구글로그인버튼 클릭 => 로그인을 완료 => code를 리턴(OAuth-Client라이브러리가 받음)
         // 코드를토대로 AcessToken을 요청
         //userRequest 정보 => 회원프로필을 받아야한다.(loadUser함수)을통해서 회원 프로필을 받을 수 있다.
